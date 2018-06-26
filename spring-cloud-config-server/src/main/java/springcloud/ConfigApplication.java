1b01730bdf73af735c8765a5f851e22331b8d23e
ASUS <ASUS@DESKTOP-B4C47LG>
2018-06-26 14:27:12 +0800

    测试springcloud

---
+++ spring-cloud-config-server/src/main/java/springcloud/ConfigApplication.java
@@ -0,0 +1,17 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		System.out.println("helloword");
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
